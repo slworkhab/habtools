c00980b72788750ea5c860652ca35d9298e7b34f
slworkhab <stephane.laurrain@habellis.fr>
2025-11-18 17:14:31 +0100

    str_to_textfile encoding

--- engine.py
+++ engine.py
@@ -28,8 +28,13 @@ class Engine:
 
 
         def read_str_file_first_line(self, file_path):        
-            with open(file_path , "r", encoding="cp1252") as file:
+            with open(file_path , "r", encoding="cp1252", errors="ignore") as file:
                 return file.readline()
+        
+        def str_to_textfile (self, filename, str_to_write):
+            text_file = open(filename, "w", encoding="cp1252")
+            text_file.write(str_to_write)
+            text_file.close()
 
         @_error_decorator()
         def copy_xlsx(self):                
@@ -40,28 +45,31 @@ class Engine:
             print (self.source_folder)
             flag_file_path = f"{self.jsprms.prms['path']['flag_copy_linux']}"
             if not os.path.exists(flag_file_path):
-                file_utils.str_to_textfile(flag_file_path, "nope")
+                self.str_to_textfile(flag_file_path, "nope")
             found_file = False
             for root, dirs, files in os.walk(self.source_folder):
                 for file in files:                    
-                    if file.lower().endswith(excel_extensions):                   
-                        if self.read_str_file_first_line(flag_file_path) == file :
-                            found_file = True
-                        if not found_file :                             
-                            source_path = os.path.join(root, file)
-                            destination_path = os.path.join(self.destination_folder, file)
-                            # If a file with the same name already exists, rename it
-                            if os.path.exists(destination_path):
-                                base, ext = os.path.splitext(file)
-                                counter = 1
-                                while os.path.exists(destination_path):
-                                    new_name = f"{base}_{counter}{ext}"
-                                    destination_path = os.path.join(self.destination_folder, new_name)
-                                    counter += 1
-                            if not os.path.exists(destination_path):
-                                shutil.copy2(source_path, destination_path)
-                                file_utils.str_to_textfile(flag_file_path, file)
-                                print(f"✅ Copied: {file}")
-                        else:
-                            print(f"Found file : {file}")
+                    if file.lower().endswith(excel_extensions):
+                        try:
+                            if self.read_str_file_first_line(flag_file_path) == file :
+                                found_file = True
+                            if not found_file :                             
+                                source_path = os.path.join(root, file)
+                                destination_path = os.path.join(self.destination_folder, file)
+                                # If a file with the same name already exists, rename it
+                                if os.path.exists(destination_path):
+                                    base, ext = os.path.splitext(file)
+                                    counter = 1
+                                    while os.path.exists(destination_path):
+                                        new_name = f"{base}_{counter}{ext}"
+                                        destination_path = os.path.join(self.destination_folder, new_name)
+                                        counter += 1
+                                if not os.path.exists(destination_path):
+                                    shutil.copy2(source_path, destination_path)
+                                    self.str_to_textfile(flag_file_path, file)
+                                    print(f"✅ Copied: {file}")
+                            else:
+                                print(f"Found file : {file}")
+                        except Exception as e:
+                                print(f"Computer malfunction : {file} {e}")  
             print("✅ All .xlsx files have been copied.")
